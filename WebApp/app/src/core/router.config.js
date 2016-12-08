14e92ac70e256efe40aa44d53a49e907c9f12391
tahir.hassan <tahir.hassan@tenpearls.com>
2016-12-08 18:51:19 +0500

    Adding background image

--- WebApp/app/src/core/router.config.js
+++ WebApp/app/src/core/router.config.js
@@ -15,12 +15,12 @@
   /* @ngInject */
   function configuration($urlRouterProvider){
 
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/login');
 
   }
 
   /* @ngInject */
-  function routingEvents($rootScope, store, $state, appUserRole){
+  function routingEvents($rootScope, store, $state){
     //on routing error
     $rootScope.$on('$stateNotFound', function(event, unfoundState, fromState, fromParams){
       //do some logging and toasting
@@ -34,9 +34,11 @@
 
     $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
         // if((store.get('user') === null && toState.role !== appUserRole.All)){
-        //     event.preventDefault();
-        //     $state.go('auth');
-        // }
+      console.log($state.current.name);
+        if($state.current.name!=="authShell.login" && store.get('user') === null){
+            event.preventDefault();
+            $state.go('authShell.login');
+        }
     });
   }
 
