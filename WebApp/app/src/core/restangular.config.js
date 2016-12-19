43266cba22273faf9a37ee10474b80bcabeea277
umair <umair.javaid@tenpearls.com>
2016-12-19 18:13:39 +0500

    restangular config updated

--- WebApp/app/src/core/restangular.config.js
+++ WebApp/app/src/core/restangular.config.js
@@ -16,12 +16,23 @@
 
     // var defaultHeaders = {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'};
     // RestangularProvider.setDefaultHeaders(defaultHeaders);
+    // RestangularProvider.setDefaultHeaders({
+    //     'Content-Type': 'application/json',
+    //     'X-Requested-With': 'XMLHttpRequest'
+    // });
+    // RestangularProvider.setDefaultHeaders({'Access-Control-Allow-Origin':'*'});
+    // RestangularProvider.setDefaultHeaders({'Access-Control-Allow-core':'*'});
+
+
     RestangularProvider.setDefaultHeaders({
-        'Content-Type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest'
+      'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+      'Access-Control-Allow-Origin':'*',
+      'Access-Control-Allow-core':'*',
+      'Access-Control-Allow-Headers':'Accept, X-Requested-With',
+      'Access-Control-Allow-Credentials':'true',
+      'Access-Control-Allow-core':'*'
     });
-    RestangularProvider.setDefaultHeaders({'Access-Control-Allow-Origin':'*'});
-    RestangularProvider.setDefaultHeaders({'Access-Control-Allow-core':'*'});
 
     RestangularProvider.setBaseUrl('/api');
   }
